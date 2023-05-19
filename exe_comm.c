c8e57f505ab32cd2ceca3a2473c5daf603ab9ade
mosetf <stanlym315@gmail.com>
2023-05-19 21:29:04 +0300

    exe_comm.c file

--- exe_comm.c
+++ exe_comm.c
@@ -1,46 +1,111 @@
 #include "shell.h"
 
 /**
-* execute_command - executes a command line input
-* @command_line: pointer to command line
-* Return: null
-*/
-void execute_command(char *command_line)
+ * exec_ute - execute a command
+ * @cmd: command to execute
+ */
+void exec_ute(char *cmd)
 {
-	char *token;
-	char *args[100];
-	int i = 0;
-	pid_t pid;
+    char *args[MAX_COMMAND_LENGTH];
+    int arg_count = 0;
 
-	token = strtok(command_line, " ");
-	while (token != NULL)
-	{
-		args[i] = token;
-		i++;
-		token = strtok(NULL, " ");
-	}
-	args[i] = NULL;
+    if (cmd[0] == '\0')
+        return;
 
-	pid = fork();
-	if (pid == -1)
-	{
-		print_error("Fork Failed");
-	}
-	else if (pid == 0)
-	{
-		execvp(args[0], args);
-		print_execvp_error();
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		int status;
+    par_se(cmd, args, &arg_count);
 
-		wait(&status);
-		if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-		{
-			print_command_not_found_error();
-		}
+    if (strcmp(args[0], "exit") == 0)
+    {
+        ex_it(args, arg_count);
+        return;
+    }
+
+    exe_com(args, program_name);
+}
+
+/**
+ * par_se - parse a command line
+ * @cmd: command line to parse
+ * @args: array to store the parsed arguments
+ * @arg_count: pointer to the argument count variable
+ */
+void par_se(char *cmd, char *args[], int *arg_count)
+{
+    int i = 0;
+
+    while (cmd[i] != '\0' && *arg_count < MAX_COMMAND_LENGTH - 1)
+    {
+        while (cmd[i] == ' ' || cmd[i] == '\t')
+            i++;
+
+        if (cmd[i] != '\0')
+        {
+            args[*arg_count] = &cmd[i];
+            (*arg_count)++;
+
+            while (cmd[i] != ' ' && cmd[i] != '\t' && cmd[i] != '\0')
+                i++;
+
+            if (cmd[i] != '\0')
+            {
+                cmd[i] = '\0';
+                i++;
+            }
+        }
+    }
+
+    args[*arg_count] = NULL;
+}
+
+/**
+ * ex_it - handle the 'exit' command
+ * @args: array of command arguments
+ * @arg_count: number of arguments
+ */
+void ex_it(char *args[], int arg_count)
+{
+    if (arg_count == 1)
+    {
+        exit(EXIT_SUCCESS);
+    }
+    else if (arg_count == 2)
+    {
+        int status = atoi(args[1]);
+        exit(status);
+    }
+    else
+    {
+        fprintf(stderr, "Usage: exit [status]\n");
+        return;
+    }
+}
+
+/**
+ * exe_com - execute a command
+ * @args: array of command arguments
+ * @program_name: name of the program
+ */
+void exe_com(char *args[], char *program_name)
+{
+    pid_t pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+    }
+    else if (pid == 0)
+    {
+        execvp(args[0], args);
+        fprintf(stderr, "%s: 1: %s: not found\n", program_name, args[0]);
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        int status;
+	waitpid(pid, &status, 0);
+        if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+	{
+		fprintf(stderr, "%s: %s: command not found\n", program_name, args[0]);
 	}
+    }
 }
 
