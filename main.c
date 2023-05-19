769737c6ca8937e0d352b3b482b5b3de41525748
mosetf <stanlym315@gmail.com>
2023-05-19 21:30:55 +0300

    update main.c file

--- main.c
+++ main.c
@@ -1,44 +1,36 @@
 #include "shell.h"
+#include "gvar.h"
+/*char *program_name; 
+char *buffer = NULL; 
+size_t bufsize = 0;
+ssize_t nread;*/
 
 /**
-* main - entry point for the shell program
-* Return: 0 on success, 1 on failure
-*/
-int main(void)
+ * main - entry point for the shell
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char **argv)
 {
-	char *buffer = NULL;
-	size_t bufsize = 0;
-	ssize_t nread;
+    (void)argc; /* Suppress unused parameter warning */
+    program_name = argv[0]; /* Assign program name from argv[0] */
 
-	while (1)
-	{
-		print_prompt();
-		nread = getline(&buffer, &bufsize, stdin);
-		if (nread == -1) /* Ctrl-D or error */
-		{
-			if (feof(stdin)) /* Ctrl-D */
-				putchar('\n');
-			else /* error */
-				perror("getline");
-			free(buffer);
-			exit(EXIT_FAILURE);
-		}
-		buffer[nread - 1] = '\0'; /* remove newline */
-		if (strcmp(buffer, "exit") == 0)
-		{
-			free(buffer);
-			exit(EXIT_SUCCESS);
-		}
-		else if (strcmp(buffer, "env") == 0)
-		{
-			check_env();
-		}
-		else
-		{
-			execute_command(buffer);
-		}
-	}
+    while (1)
+    {
+        print_prompt();
+        buffer = get_line();
+        if (buffer == NULL)
+        {
+            putchar('\n');
+            free(buffer);
+            exit(EXIT_SUCCESS);
+        }
+        /* Execute command */
+        exec_ute(buffer);
+        free(buffer);
+    }
 
-	return (0);
+    return 0;
 }
 
