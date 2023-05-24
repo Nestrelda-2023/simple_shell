bbe73948795d9e2db6adcff23946d74c7f7e20ce
mosetf <stanlym315@gmail.com>
2023-05-24 19:03:15 +0300

    update main.c

--- main.c
+++ main.c
@@ -1,33 +1,32 @@
 #include "shell.h"
 #include "gvar.h"
 
-
 /**
- * main - entry point for the shell
- * @argc: argument count
- * @argv: argument vector
+ * main - Entry point for the shell
+ * @argc: Argument count
+ * @argv: Argument vector
  * Return: 0 on success, 1 on error
  */
 int main(int argc, char **argv)
 {
-    (void)argc; /* Suppress argc*/
-    program_name = argv[0]; /* Assign program name from argv[0] */
+	(void)argc;
+	program_name = argv[0];
 
-    while (1)
-    {
-        print_prompt();
-        buffer = get_line();
-        if (buffer == NULL)
-        {
-            putchar('\n');
-            free(buffer);
-            exit(EXIT_SUCCESS);
-        }
-        /* Execute command */
-        exec_ute(buffer);
-        free(buffer);
-    }
+	while (1)
+	{
+		print_prompt();
+		buffer = get_line();
+		if (buffer == NULL)
+		{
+			putchar('\n');
+			free(buffer);
+			exit(EXIT_SUCCESS);
+		}
 
-    return 0;
+		exec_ute(buffer);
+		free(buffer);
+	}
+
+	return (0);
 }
 
