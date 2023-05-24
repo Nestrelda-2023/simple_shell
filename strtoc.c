ec5f18901e84551e310d61cd45e09f41c59686b8
mosetf <stanlym315@gmail.com>
2023-05-24 19:17:54 +0300

    implement strtoc.c file

---
+++ strtoc.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+/**
+ * str_toc - Tokenize a string into consecutive tokens
+ * @str: The string to tokenize
+ * @delim: The delimiter character(s)
+ * @saveptr: Pointer to the saved position for subsequent calls
+ *
+ * Return: Pointer to the next token in the string
+ */
+char *str_toc(char *str, const char *delim, char **saveptr)
+{
+	char *token_start;
+	char *token_end;
+	if (str != NULL)
+	{
+		*saveptr = str;
+	}
+	else
+	{
+		if (*saveptr == NULL)
+		{
+			return (NULL);
+		}
+		str = *saveptr;
+	}
+
+	token_start = str;
+	token_end = str_pbrk(token_start, delim);
+
+	if (token_end != NULL)
+	{
+		*token_end = '\0';
+		*saveptr = token_end + 1;
+	}
+	else
+	{
+		*saveptr = NULL;
+	}
+
+	return (token_start);
+}
+
