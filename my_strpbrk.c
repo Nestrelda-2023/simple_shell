72b84ecdaba64291971f4081f0095440fb5387d2
mosetf <stanlym315@gmail.com>
2023-05-24 19:16:18 +0300

    update my_strpbrk.c file

---
+++ my_strpbrk.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+/**
+ * str_pbrk - Searches a string for any set of bytes
+ * @str: string to be searched
+ * @delim: bytes to be searched
+ *
+ * Return: Pointer to the byte in str that matches one of the bytes in delim
+ *         else NULL
+ */
+char *str_pbrk(const char *str, const char *delim)
+{
+	const char *s1 = str;
+	const char *s2;
+
+	while (*s1 != '\0')
+	{
+		s2 = delim;
+		while (*s2 != '\0')
+		{
+			if (*s1 == *s2)
+				return ((char *)s1);
+			s2++;
+		}
+		s1++;
+	}
+
+	return (NULL);
+}
+
